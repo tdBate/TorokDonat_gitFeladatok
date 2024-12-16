14d5ed69bcc8b0485815b915cfa3705e4a540f0c
Torok Donat <td.zazsa@gmail.com>
2024-12-16 15:23:28 +0100

    F01 feladat elkészítve

--- Program.cs
+++ Program.cs
@@ -8,6 +8,19 @@ namespace TorokDonat_gitFeladatok
 {
     internal class Program
     {
+        static void F1(int[] tomb)
+        {
+            Random rnd = new Random();
+
+            for (int a = 0; a < tomb.Length; a++)
+            {
+                tomb[a] = rnd.Next(1,51);
+                Console.WriteLine(tomb[a]);
+            }
+
+        }
+
+
         static void Main(string[] args)
         {
         }
