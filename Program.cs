8a226dbe2ee01ada006c5ec7ea64a5d5dd5bc7db
Torok Donat <td.zazsa@gmail.com>
2024-12-16 15:31:06 +0100

    F02 feladat elkészítve

--- Program.cs
+++ Program.cs
@@ -11,15 +11,22 @@ namespace TorokDonat_gitFeladatok
         static void F1(int[] tomb)
         {
             Random rnd = new Random();
-
             for (int a = 0; a < tomb.Length; a++)
             {
                 tomb[a] = rnd.Next(1,51);
                 Console.WriteLine(tomb[a]);
             }
-
         }
 
+        static int F2(int[] tomb)
+        {
+            int szamlalo = 0;
+            for (int a = 0; a < tomb.Length; a++)
+            {
+                if (tomb[a] % 2 == 0) { szamlalo++; }
+            }
+            return szamlalo;
+        }
 
         static void Main(string[] args)
         {
